767f8cc7f0bf5a4ba394ce2ce680c1061ccc7f3a
Sully Bck <sullyvan.lb@gmail.com>
2026-07-01 02:40:23 +0200

    padding

--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -7,5 +7,5 @@ export function Section({
   children: ReactNode;
   className?: string;
 }) {
-  return <section className={`py-20 md:py-28 ${className}`}>{children}</section>;
+  return <section className={`py-14 md:py-20 ${className}`}>{children}</section>;
 }
